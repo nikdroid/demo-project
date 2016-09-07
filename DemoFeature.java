8a68bdf57830703cf9e43b08281498df8a270b32
nikdroid <nikdroid12@gmail.com>
2016-09-08 02:43:01 +0530

    Demo Feature

---
+++ DemoFeature.java
@@ -0,0 +1,10 @@
+public class DemoFeature{
+
+     public static void main(String []args){
+        System.out.println("Hello Feature");
+     }
+	 
+	 private static String getName(){
+		 return "Name";
+	 }
+}
