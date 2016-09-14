981b2e683db1df1078a53e7f8d8651036a9354f1
nikdroid <nikdroid12@gmail.com>
2016-09-14 23:31:20 +0530

    fibonacci

--- DemoFeature.java
+++ DemoFeature.java
@@ -1,4 +1,6 @@
 public class DemoFeature{
+	
+	
 
      public static void main(String []args){
         System.out.println("Hello Feature");
@@ -7,4 +9,14 @@ public class DemoFeature{
 	 private static String getName(){
 		 return "Name";
 	 }
+	 
+	private static int fibonacci(int n){
+		if(n == 0)
+			return 0;
+		else if(n == 1)
+			return 1;
+		else
+			return fibonacci(n - 1) + fibonacci(n - 2);
+	}
 }
+
