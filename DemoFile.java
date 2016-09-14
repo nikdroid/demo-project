78296910c3cfd0046bf98ef87435c564c3bcee56
nikdroid <nikdroid12@gmail.com>
2016-09-14 23:51:25 +0530

    demo file changes

--- DemoFile.java
+++ DemoFile.java
@@ -1,10 +1,14 @@
 public class DemoFile{
 
      public static void main(String []args){
-        System.out.println("Hello World");
+        System.out.println("Hello World!!!");
      }
 	 
 	 private static String getName(){
 		 return "Name";
+	 }
+	 
+	 private static String getDates(){
+		 return "date";
 	 }
 }
